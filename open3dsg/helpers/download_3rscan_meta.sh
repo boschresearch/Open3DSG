9c5d8e0f352dc470b562e8325ee65e98454c3337
Koch Sebastian (CR/AAS3) <eok3fe@bosch.com>
2024-08-19 09:05:32 +0200

    update meta download links

--- open3dsg/helpers/download_3rscan_meta.sh
+++ open3dsg/helpers/download_3rscan_meta.sh
@@ -2,10 +2,10 @@
 # Copyright (c) 2024 Robert Bosch GmbH
 # SPDX-License-Identifier: AGPL-3.0
 
-wget http://campar.in.tum.de/files/3RScan/rescans.txt
-wget http://campar.in.tum.de/files/3RScan/train_scans.txt
-wget http://campar.in.tum.de/files/3RScan/val_scans.txt
-wget http://campar.in.tum.de/files/3RScan/3RScan.json
+wget https://campar.in.tum.de/public_datasets/3RScan/rescans.txt
+wget https://campar.in.tum.de/public_datasets/3RScan/train_scans.txt
+wget https://campar.in.tum.de/public_datasets/3RScan/val_scans.txt
+wget https://campar.in.tum.de/public_datasets/3RScan/3RScan.json
 
 pip install gdown
 gdown --folder https://drive.google.com/drive/folders/1onEfhRDZWFvVsrwcEa3jTr3m-otYTFty
